41ac9dea4c14d2f873901d5b1fb5e2580c3d181b
Zoro9-star <pradhannanda547@gmail.com>
2026-06-27 21:11:28 +0530

    Added QuesNo 152 - Maximum Sum Subarray

--- Kadane Algorithm/LeetCode_152.py
+++ Kadane Algorithm/LeetCode_152.py
@@ -1,3 +1,22 @@
+# Ques No 53 - Maximum Subarray
+
+# Problem Statement:
+# Given an integer array nums, find the with the largest sum, and return its sum.
+
+
+# Example 1:
+
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output: 6
+# Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+# Example 2:
+
+# Input: nums = [1]
+# Output: 1
+# Explanation: The subarray [1] has the largest sum 1.
+
+
 class Solutionn:
     def maxProduct(self, nums):
         best = nums[0]
