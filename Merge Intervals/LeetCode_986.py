a6ecdcc71a12212dee7601ed7990e213677739c9
Zoro9-star <pradhannanda547@gmail.com>
2026-07-07 16:58:50 +0530

    Added QuesNo 986 - Inteval List Intersection

---
+++ Merge Intervals/LeetCode_986.py
@@ -0,0 +1,49 @@
+# Ques No 986 - Interval List Intersection
+
+# Problem Statement:
+# You are given two lists of closed intervals, firstList and secondList, where firstList[i] = [starti, endi] and secondList[j] = [startj, endj]. Each list of intervals is pairwise disjoint and in sorted order.
+
+# Return the intersection of these two interval lists.
+
+# A closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.
+
+# The intersection of two closed intervals is a set of real numbers that are either empty or represented as a closed interval. For example, the intersection of [1, 3] and [2, 4] is [2, 3].
+
+ 
+
+# Example 1:
+
+# Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],[15,24],[25,26]]
+# Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+
+# Example 2:
+
+# Input: firstList = [[1,3],[5,9]], secondList = []
+# Output: []
+
+
+class Solution:
+    def intervalIntersection(self, firstList, secondList):
+        i , j , res = 0,0,[]
+        while i < len(firstList) and j < len(secondList):
+            start1 = firstList[i][0]
+            end1 = firstList[i][1]
+            start2 = secondList[j][0]
+            end2 = secondList[j][1]
+            if start1 <= start2:
+                if end1 >= start2:
+                    s = max(start1,start2)
+                    e = min(end1 , end2)
+                    res.append([s,e])
+            else:
+                if end2 >= start1:
+                    s = max(start1,start2)
+                    e = min(end1,end2)
+                    res.append([s,e])
+            if end1 <= end2:
+                i+=1
+            else:
+                j+=1
+        return res
+obj = Solution()
+print(obj.intervalIntersection([[0,2],[5,10],[13,23],[24,25]],[[1,5],[8,12],[15,24],[25,26]]))
